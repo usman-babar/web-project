22fd7452daf0c376d2004642a918b1351e9de43e
usman-babar <usmanbabar1909@gmail.com>
2023-05-07 01:34:28 +0500

    updating Schema

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
-
 import increment from "mongoose-auto-increment";
 
 const managerSchema = mongoose.Schema({
   name: { type: String, required: [true, 'Enter the name'], trim: true },
-  gender: { type: String, required: true, lowercase: true, enum: ["male", "female"] },
-  address: { type: String, required: true },
+  // gender: { type: String, required: true, lowercase: true, enum: ["male", "female"] },
+  // address: { type: String, required: true },
   date_of_birth: { type: String, required: true, minlength: 6 },
   username: { type: String, unique: true, required: true },
   password: { type: String, maxlength: 10, required: true, unique: true },
   phone_number: { type: String, required: true },
-  role: { type: String, lowercase: true, required: true },
+  // role: { type: String, lowercase: true, required: true },
   cnic_number: { type: String, unique: true, maxlength: 12, required: true },
   creation_date: { type: Date, default: Date.now() }
-
 });
 
 increment.initialize(mongoose.connection);
